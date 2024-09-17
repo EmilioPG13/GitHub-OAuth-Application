0826766b4de0587107cad86ab20b6ea4837b5dff
Emilio <emilio.pg95@gmail.com>
2024-09-16 18:52:02 -0600

    Add session and passport configuration

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require("path");
 require("dotenv").config();
 const express = require('express');
 const partials = require('express-partials');
+const session = require("express-session");
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 
 const app = express();
@@ -23,10 +26,22 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
  * Passport Configurations
 */
 
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+function(accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}));
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-
-
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 
 /*
@@ -39,7 +54,14 @@ app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+  secret: 'codecademy',
+  resave: false,
+  saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 /*
